190cd7f65ba083b9e94714a159766f47b4de505d
helinea <57397805+helinea@users.noreply.github.com>
2024-10-30 08:52:08 +0000

    Configure dev vscode browser launch

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ class WeatherApp(Reflex):
     def get_weather(self):
         if not self.city:
             return
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}&units=metric"
+        url = f"http://opendata.fmi.fi/wfs"#;http://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}&units=metric"
         response = requests.get(url)
         if response.status_code == 200:
             self.weather_data = response.json()
         else:
             self.weather_data = None
+
 
     def render(self):
         self.clear()
